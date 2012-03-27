223566d325c20b0f78d507907a859b1b4ec90bc3
Robert Rees <robert@wazoku.com>
2012-03-27 20:01:40 +0100

    Hello world

---
+++ hello.js
@@ -0,0 +1,2 @@
+goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
+goog.addDependency("../wgfqw.js", ['hello'], ['cljs.core']);
