fce1a6803d604863e6768f3754269530e42157ca
Robert Rees <robert@wazoku.com>
2012-03-27 20:43:17 +0100

    Working sampling

--- drawing.cljs
+++ drawing.cljs
@@ -3,5 +3,6 @@
 (defn ^:export test [] "Hello from draw")
 
 (defn ^:export draw [canvas]
+	(let [x (rand-int 400) y (rand-int 400)]
 	(set! (.-fillStyle canvas) "rgb(200, 0, 0)")
-	(. canvas fillRect 10 10 55 50))
+	(. canvas fillRect x y 55 50)))
--- drawing.js
+++ drawing.js
@@ -1,2 +1,2 @@
 goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.string.StringBuffer', 'goog.object', 'goog.array']);
-goog.addDependency("../YAyda.js", ['drawing'], ['cljs.core']);
+goog.addDependency("../D2OxI.js", ['drawing'], ['cljs.core']);
